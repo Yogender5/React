071b247dc2f556f20463a2fb10b3a007be7f1d56
Yogender Malladi <31113626+Yogender5@users.noreply.github.com>
2020-06-23 20:56:32 -0700

    first push

---
+++ src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1> Hello Yogi </h1>
+// } ES5 syntax
+
+const Greet = (props) => {
+    console.log(props)
+    return (
+        <div>
+        <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+        {props.children}
+        </div>
+        )
+}
+export default Greet
